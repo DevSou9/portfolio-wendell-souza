f00ef84a352bb9970d0d3db92e61de2f57a44f11
W Souza <dev9.ws@gmail.com>
2024-04-24 21:26:01 -0300

    feature/links

--- src/components/Descricao/index.tsx
+++ src/components/Descricao/index.tsx
@@ -26,17 +26,36 @@ export const Descricao = () => {
           <div className="divContato">
             <div className="divContatoElementos">
               <MdAlternateEmail style={{ color: colors.blue }} />
-              <p>contato@wendellsouza.com</p>
+              <a
+                href="mailto:contato@wendellsouza.com"
+                target="_blank"
+                rel="noreferrer"
+              >
+                {' '}
+                <p>contato@wendellsouza.com</p>
+              </a>
             </div>
 
             <div className="divContatoElementos">
               <MdOutlineWhatsapp style={{ color: colors.blue }} />
-              <p>(21) 99609-3012</p>
+              <a
+                href="https://wa.me/5521996093012"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <p>(21) 99609-3012</p>
+              </a>
             </div>
 
             <div className="divContatoElementos">
               <CiLinkedin className="icon" style={{ color: colors.blue }} />
-              <p>https://www.linkedin.com/in/wendell-souza/</p>
+              <a
+                href="https://www.linkedin.com/in/wendell-souza/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <p>https://www.linkedin.com/in/wendell-souza/</p>
+              </a>
             </div>
           </div>
         </div>
